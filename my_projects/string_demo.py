8c2b5844078d1ad4dd325898e81a5f62fe9a648f
Vasileios2310 <vasileioskritikos@aueb.gr>
2025-12-28 14:17:03 +0200

    new project

--- my_projects/string_demo.py
+++ my_projects/string_demo.py
@@ -53,14 +53,55 @@ python_books = {
     "Python Crash Course by Eric Matthes"
 }
 
-
+print("------------1----------")
 # Έκφραση που επιστρέφει σύνολο από όλα τα βιβλία της Python που δεν έχουν ως θέμα την τεχνητή νοημοσύνη.
-
 str_python = 'Artificial Intelligence'
 python_books_without_AI = set()
 for item in python_books:
     if str_python not in item:
         python_books_without_AI.add(item)
 print(python_books_without_AI)
-    
+
+print("------------2----------")  
 # Έκφραση που επιστρέφει σύνολο από όλα τα βιβλία της  Java που έχουν ως θέμα την τεχνητή νοημοσύνη. 
+str_java = 'Artificial Intelligence'
+java_books_with_AI = set()
+for item in java_books:
+    if str_java in item:
+        java_books_with_AI.add(item)
+print(java_books_with_AI)
+
+print("------------3----------")
+# Έκφραση που επιστρέφει σύνολο από όλα τα βιβλία της βιβλιοθήκης που δεν αφορούν την τεχνητή νοημοσύνη.
+str_out = 'Artificial Intelligence'
+books_without_AI = python_books.union(ai_books).union(java_books)
+lib_books_without_AI = set()
+for item in books_without_AI:
+    if str_out not in item:
+        lib_books_without_AI.add(item)
+print(lib_books_without_AI)
+
+
+print("------------4----------")
+# Έκφραση που επιστρέφει σύνολο από όλα τα βιβλία της βιβλιοθήκης που είναι είτε για τεχνητή νοημοσύνη είτε για Python αλλά όχι και τα δύο
+str_out = 'Artificial Intelligence'
+books_union = python_books.union(ai_books)
+lib_books_with_AI_or_Python = set()
+for item in books_union:
+    if str_out not in item:
+        lib_books_with_AI_or_Python.add(item)
+print(lib_books_with_AI_or_Python)
+
+
+print("------------5----------")
+# Έστω book μια συμβολοσειρά που περιέχει το όνομα του βιβλίου και του συγγραφέα όπως στα παραδείγματα που προηγήθηκαν
+# (π.χ. "Python Crash Course by Eric Matthes").
+# Γράψτε την έκφραση, στην book[έκφραση],  που απομονώνει το όνομα του συγγραφέα (π.χ. "Eric Matthes") από τη συμβολοσειρά. 
+# Υπόδειξη: κάνετε χρήση της συνάρτησης find με το κατάλληλο όρισμα και δημιουργήστε μία έκφραση που να την περιέχει,
+# ώστε να απομονώσετε το όνομα του συγγραφέα για κάθε πιθανή συμβολοσειρά που δόθηκε.
+
+java_books_with_AI = set()
+for item in java_books:
+    index = item.find("by")
+    str_name = item[index:]
+    print(str_name)
