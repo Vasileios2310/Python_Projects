04a18a3e0af90d0e7807b934737f575e4c362733
Vasileios2310 <vasileioskritikos@aueb.gr>
2026-02-02 20:14:43 +0200

    map_filter

--- chapter04/16_map_filter_demo.py
+++ chapter04/16_map_filter_demo.py
@@ -1,5 +1,18 @@
 cities = ["london" , "paris" , "barcelona" , "athens"]
 
+# filter city names ( longer than 5 characters)
 long_city = list(filter(lambda city : len(city) > 5 , cities))
-
 print(f"long cities > 5 characters , {long_city}")
+
+# capitalized cities
+cap_cities = list(map(lambda city : city.title() , cities))
+print(f"cap cities : {cap_cities}")
+
+
+# capitilized all long cities
+cap_long_cities = list(map(lambda city : city.title() , long_city))
+print(f"cap long cities  : {cap_long_cities}")
+
+
+cap_length_cities = list(map(lambda city : city.title() , filter(lambda city : len(city) > 5 , cities)))
+print(f"cap long cities  : {cap_length_cities}")
