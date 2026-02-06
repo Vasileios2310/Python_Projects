f5a561ce3b0ad146178f3569e4e52ba267ac63ef
Vasileios2310 <vasileioskritikos@aueb.gr>
2026-02-06 19:04:48 +0200

    enrole project

--- chapter05/05_student_enrol.py
+++ chapter05/05_student_enrol.py
@@ -1,9 +1,9 @@
 def enroll_students(*students , min_grade = 50 , department = "Computer Science" , **kwargs):
     
     print(f"Min grade : {min_grade}")
-    print(f"Department : {department}")\
+    print(f"Department : {department}")
         
-    print("\Enrolled Students:")
+    print("Enrolled Students:")
     for student in students:
         print(f" - {student}")
     
@@ -11,12 +11,20 @@ def enroll_students(*students , min_grade = 50 , department = "Computer Science"
     print("Additional Information")
     for key , val in kwargs.items():
         print(f"{key} : {val}")
+        
+    print('-----End of enrolement------')
 
 
 
 def main():
     
+    enroll_students("Alice" , "Bob")
     
-    pass
+    enroll_students("Bill" , "Helen" , "Sia" , academic_year = 2026 , semester = "Fall")
+    
+    enroll_students("Dalton" , min_grade=40 , department="Theater")
+    
+    enroll_students("John" , "Dave" , min_grade=70 , department="STEM" , academic_year = 2026 , semester = "Spring")
+    
 if __name__ == '__main__':
     main()
